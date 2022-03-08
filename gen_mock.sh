dc9fc0a465bf69d463d1f0a06f951749aee1f541
OmarAouini <aouini.omar93@gmail.com>
2022-03-08 21:01:49 +0100

    unit test with mocks ok

--- gen_mock.sh
+++ gen_mock.sh
@@ -1,3 +1,7 @@
+#need to install mockery
+# go install github.com/vektra/mockery/v2@latest
+# or
+# brew install mockery
 
 mockery --recursive --name=CompanyStore
 mockery --recursive --name=EmployeeStore
