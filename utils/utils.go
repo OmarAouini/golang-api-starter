eb42dc33cc59e0125f44fddf3fef0c2408158175
OmarAouini <aouini.omar93@gmail.com>
2022-03-10 00:51:01 +0100

    cleanup

--- utils/utils.go
+++ utils/utils.go
@@ -2,16 +2,9 @@ package utils
 
 import (
 	"encoding/json"
-
-	"github.com/sirupsen/logrus"
 )
 
 func PrettyPrint(i interface{}) string {
 	s, _ := json.MarshalIndent(i, "", "\t")
 	return string(s)
-}
-
-func PrettyPrintDebug(message, i interface{}) {
-	s, _ := json.MarshalIndent(i, "", "\t")
-	logrus.Debugf("%s: %s", message, string(s))
 }
