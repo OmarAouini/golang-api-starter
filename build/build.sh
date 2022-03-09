3c2f95d40ee202d9b050a1417ef33f0f22bd517d
OmarAouini <aouini.omar93@gmail.com>
2022-03-09 23:53:42 +0100

    added bin folder for build binary of project

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,2 @@
 echo "building..."
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "api" -ldflags="-s -w" *.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o "./build/bin/api" -ldflags="-s -w" *.go
